57bd050d2ebbf07c985ec0dcfd975abba9c9239f
이예지 <oyatpeach@naver.com>
2024-07-28 14:01:45 +0900

    feat : city, country, travel, like 엔티티 생성 #1

---
+++ src/main/java/umc/TripPiece/domain/common/City.java
@@ -0,0 +1,28 @@
+package umc.TripPiece.domain.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+public class City {
+    @Id @GeneratedValue
+    @Column(name = "city_id")
+    private BigInteger id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    private String name;
+    private String comment;
+    private String cityImage;
+    private BigInteger logCount;
+
+    @OneToMany(mappedBy = "city")
+    private List<Travel> travels = new ArrayList<>();
+}
---
+++ src/main/java/umc/TripPiece/domain/common/Like.java
@@ -0,0 +1,23 @@
+package umc.TripPiece.domain.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.math.BigInteger;
+
+@Entity
+@Getter
+public class Like {
+    @Id @GeneratedValue
+    @Column(name = "like_id")
+    private BigInteger id;
+
+    //    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user; 회원 엔티티 만들면 연결
+
+   @ManyToOne(fetch = FetchType.LAZY)
+   @JoinColumn(name = "travel_id")
+   private Travel travel;
+
+}
