ab6b1968210cf372a875e77ab3d209a572c2bcdb
이예지 <oyatpeach@naver.com>
2024-08-01 14:05:34 +0900

    [feature] CityConverter 추가 및 CityService 로직 변경 #10

---
+++ src/main/java/umc/TripPiece/converter/CityConverter.java
@@ -0,0 +1,16 @@
+package umc.TripPiece.converter;
+
+import umc.TripPiece.domain.City;
+import umc.TripPiece.web.dto.response.CityResponseDto;
+
+public class CityConverter {
+    public static CityResponseDto.searchDto toSearchDto(City city){
+        return new CityResponseDto.searchDto(
+                city.getName(),
+                city.getCountry().getName(),
+                city.getComment(),
+                city.getCityImage(),
+                city.getLogCount()
+        );
+    }
+}
