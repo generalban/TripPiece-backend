d7874a077c8f249358845de04c7559d1eb2f1a5b
이예지 <oyatpeach@naver.com>
2024-08-05 14:59:42 +0900

    [fix] travel_seq 에러 해결

--- src/main/java/umc/TripPiece/domain/Travel.java
+++ src/main/java/umc/TripPiece/domain/Travel.java
@@ -16,7 +16,7 @@ import java.util.List;
 @AllArgsConstructor
 public class Travel extends BaseEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "travel_id")
     private Long id;
 
