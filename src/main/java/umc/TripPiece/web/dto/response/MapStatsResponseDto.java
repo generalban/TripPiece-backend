9236acf5c40570f70d28b64db56871c26bfa3bb4
soungjunban <soungjunban@gmail.com>
2024-08-22 17:44:43 +0900

    [feature] get profileimg,nickname

--- src/main/java/umc/TripPiece/web/dto/response/MapStatsResponseDto.java
+++ src/main/java/umc/TripPiece/web/dto/response/MapStatsResponseDto.java
@@ -1,17 +1,23 @@
 package umc.TripPiece.web.dto.response;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Data
+@Getter
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class MapStatsResponseDto {
+
     private long countryCount;
     private long cityCount;
-
-    // 추가: 국가 코드와 도시 ID 리스트
     private List<String> countryCodes;
     private List<Long> cityIds;
+    private String profileImg;   // 프로필 이미지
+    private String nickname;     // 닉네임
+
 }
